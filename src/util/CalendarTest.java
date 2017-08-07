6d374c0658507b8ce96e23c72b08346306ab6e69
YeonJin <duswls0515@gmail.com>
2017-08-07 11:05:35 +0900

    17.08.07 11:05

---
+++ src/util/CalendarTest.java
@@ -0,0 +1,48 @@
+package util;
+
+import java.util.Calendar;
+
+public class CalendarTest {
+	
+	public static void main(String[] args) {
+		Calendar cal = Calendar.getInstance();
+		printDate(cal);
+		Calendar cal2 = Calendar.getInstance();
+		cal2.set(Calendar.YEAR, 2015);
+		cal2.set(Calendar.MONTH, 12);
+		cal2.set(Calendar.DATE, 15);
+		
+		cal2.add(Calendar.DATE, 100);
+		printDate(cal2);
+		
+		cal2.set(2015,12,15);
+		cal2.add(Calendar.DATE, 10000);
+		printDate(cal2);
+	}
+	
+	public static void printDate(Calendar cal) {
+		
+		String[] days = {
+				"일","월","화","수","목","금","토",
+		};
+		
+		//year은 아까처럼 + 1900 이런거 안해도 됨
+		int year = cal.get(Calendar.YEAR);
+		//0~11이니까 + 1해서 출력해야 함
+		int month = cal.get(Calendar.MONTH);
+		int date = cal.get(Calendar.DATE);
+		//1(일) ~ 7(토) ==> 월요일부터가 아닌걸 기억하기
+		//그리고 1부터니까 -1 해줘야함
+		int day = cal.get(Calendar.DAY_OF_WEEK);
+		
+		int am = cal.getMaximum(Calendar.AM_PM);
+		int hour = cal.get(Calendar.HOUR);
+		int min = cal.get(Calendar.MINUTE);
+		int sec = cal.get(Calendar.SECOND);
+		
+		System.out.println(year + "-" + (month + 1) + "-" + date + "  (" +days[day-1]+ ") "+
+				+ (am ==0? "am" : "pm") + hour + ":" + min);
+		
+	}
+
+}
